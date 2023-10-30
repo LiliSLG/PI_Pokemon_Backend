213a56c92116e0078624ada2b697d1e202c8b507
LiliSLG <silvialilianagarcia@hotmail.com>
2023-10-30 18:28:54 -0300

    use sslmode=require in your database connection configuration

--- index.js
+++ index.js
@@ -17,13 +17,17 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+require("dotenv").config();
+const { PORT } = process.env;
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
+// force:true - ELIMINA TODAS LAS TABLAS DE LA BDD, Y LAS VUELVE A CREAR EN BASE A LOS MODELOS
+// alter:true - ACTUALIZA LAS TABLAS DE BDD EN BASE A LOS MODELOS
 // conn.sync({ force: true }).then(() => {
 conn.sync({ alter: true }).then(() => {
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log("Server raised int port: " +PORT); // eslint-disable-line no-console
   });
 });
