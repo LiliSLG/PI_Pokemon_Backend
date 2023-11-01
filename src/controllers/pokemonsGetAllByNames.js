522d45571285f91ab73a98c9632bb51da69f1db7
LiliSLG <silvialilianagarcia@hotmail.com>
2023-10-31 22:59:01 -0300

    Search names

--- src/controllers/pokemonsGetAllByNames.js
+++ src/controllers/pokemonsGetAllByNames.js
@@ -11,22 +11,32 @@ const pokemonsGetAllByNames = async () => {
     attributes: ["name", [Sequelize.literal("false"), "created"]],
   });
   if (pokemonsByNameFromAPI.length === 0) {
-    const pokemonsRawFromAPI = await axios(`${API_URL}/pokemon?limit=1292`);
-    const pokemonURLRequestsAPI = pokemonsRawFromAPI.data?.results.map(
-      (pokemon) => axios.get(pokemon.url)
-    );
-    const pokemonUrlResponsesAPI = await Promise.all(pokemonURLRequestsAPI);
-    const pokemonDataFromAPI = pokemonUrlResponsesAPI.map(
-      (response) => response.data
-    );
-    const sortedPokemonsFromAPI = pokemonMapNames(pokemonDataFromAPI).sort(
-      (a, b) => a.name.localeCompare(b.name) // Sort by name in ascending order
-    );
-    await PokemonName.bulkCreate(sortedPokemonsFromAPI);
-    pokemonsByNameFromAPI = await PokemonName.findAll({
-      attributes: ["name", [Sequelize.literal("false"), "created"]],
-    });
+    for (let index = 0; index <= 200; index++) {
+      const offset = index * 100 + 1; // Calculate the offset based on the current page
+      const limit = 100; // Set the limit to the page size
+
+      const pokemonsRawFromAPI = await axios.get(`${API_URL}/pokemon`, {
+        params: {
+          offset,
+          limit,
+        },
+      });
+      const pokemonURLRequestsAPI = pokemonsRawFromAPI.data?.results.map(
+        (pokemon) => axios.get(pokemon.url)
+      );
+      const pokemonUrlResponsesAPI = await Promise.all(pokemonURLRequestsAPI);
+      const pokemonDataFromAPI = pokemonUrlResponsesAPI.map(
+        (response) => response.data
+      );
+      const sortedPokemonsFromAPI = pokemonMapNames(pokemonDataFromAPI).sort(
+        (a, b) => a.name.localeCompare(b.name) // Sort by name in ascending order
+      );
+      await PokemonName.bulkCreate(sortedPokemonsFromAPI);
+    }
   }
+  pokemonsByNameFromAPI = await PokemonName.findAll({
+    attributes: ["name", [Sequelize.literal("false"), "created"]],
+  });
   const pokemonsByNameFromDB = await Pokemon.findAll({
     attributes: ["name", [Sequelize.literal("true"), "created"]],
   });
