209506563cd039b6b824cc53ee62886e13f3782a
LiliSLG <silvialilianagarcia@hotmail.com>
2023-11-14 09:15:55 -0300

    registro

--- src/handlers/userHandlerPost.js
+++ src/handlers/userHandlerPost.js
@@ -1,16 +1,23 @@
 const { userPost } = require("../controllers");
 
-const userHandlerPost = async (req, res) => {
+const userHandlerPost =   async (req, res) => {
   try {
     const { eMail, password, fullName } = req.body;
+    
     const user = await userPost(fullName, eMail, password);
+    
+    if (user) {
+      // hago el login.
+      const token = await userLogin(eMail, password);      
+      res.status(201).json({ token });
+    }
+    
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
-    //{ message: 'An error occurred while registering the user' }
     return res.status(500).json({ error: error.message });
   }
 };
 
 module.exports = {
-    userHandlerPost,
+  userHandlerPost,
 };
